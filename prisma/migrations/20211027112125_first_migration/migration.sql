87f42f4709990c2ca39979a6b47324d1b91f46c4
Shamin Asfaq <shamin.asfaq@gmail.com>
2021-10-27 17:31:22 +0600

    first commit

---
+++ prisma/migrations/20211027112125_first_migration/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "EntryLog" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "start" DATETIME NOT NULL,
+    "end" DATETIME NOT NULL,
+    "user" TEXT NOT NULL
+);
